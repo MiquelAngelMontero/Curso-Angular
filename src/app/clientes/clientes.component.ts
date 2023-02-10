473248add83ef5776cdc353abe41cbde19d922c7
MiquelAngelMontero <gitmmontero@gmail.com>
2023-02-10 13:59:16 +0100

    classe21

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
+import { ClienteService } from './cliente.service';
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html'
 })
-export class ClientesComponent {
+export class ClientesComponent implements OnInit {
+
+  clientes: Cliente[];
+
+  constructor(private clienteService: ClienteService) {
+
+  }
+
+  ngOnInit(): void {
+    this.clientes=this.clienteService.getClientes();
+  }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,10 @@
+import { Cliente } from "./cliente";
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Miquel', apellido: 'Montero', email: 'mmontero@xipset.net', createAt: '2002-11-09'},
+    {id: 2, nombre: 'David', apellido: 'Soler', email: 'dsoler@xipset.net', createAt: '1993-07-19'},
+    {id: 3, nombre: 'David', apellido: 'Cervantes', email: 'dcervantes@xipset.net', createAt: '1982-05-10'},
+    {id: 4, nombre: 'Marc', apellido: 'Ortiz', email: 'mortiz@xipset.net', createAt: '1999-06-19'},
+    {id: 5, nombre: 'Diego', apellido: 'Burgos', email: 'dburgos@xipset.net', createAt: '2002-10-19'},
+    {id: 6, nombre: 'Adrià', apellido: 'Arce', email: 'aarce@xipset.net', createAt: '1999-08-20'}
+];
